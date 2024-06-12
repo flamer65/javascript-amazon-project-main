64e23301ad2b65db432b148ca193d44c23a41a17
flaming65 <namank6510@gmail.com>
2024-06-12 23:04:30 +0530

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: '2'
+},{
+    productId: `54e0eccd-8f36-462b-b68a-8182611d9add`,
+    quantity: '3'
+}];
 export function addToCart(productId){
     let matchingItem;
     cart.forEach((cartitem) => {
